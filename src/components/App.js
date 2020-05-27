e4db2b1a45e21c2a681acb8071e9181336affc28
Fer Torres <fertorresmx@gmail.com>
2020-05-27 02:24:30 -0500

    Add first function using React

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,23 @@
 import React from "react";
 
 const App = () => {
+
+  const ponerFilas = () => [
+    <tr>
+      <td>Fernando</td>
+      <td>fertorresmx@gmail.com</td>
+      <td>@fertorresmx</td>
+    </tr>,
+    <tr>
+      <td>Alejandro</td>
+      <td>fertorresmx2@gmail.com</td>
+      <td>@alejandro</td>
+    </tr>
+  ];
+
   return (
-		<div className='margen'>
-			<table className='tabla'>
+		<div className = 'margen'>
+			<table className = 'tabla'>
         <thead>
           <tr>
             <th>Nombre</th>
@@ -12,16 +26,7 @@ const App = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Fernando</td>
-            <td>fertorresmx@gmail.com</td>
-            <td>@fertorresmx</td>
-          </tr>
-          <tr>
-            <td>Alejandro</td>
-            <td>fertorresmx2@gmail.com</td>
-            <td>@alejandro</td>
-          </tr>
+          { ponerFilas() }
         </tbody>
       </table>
     </div>
